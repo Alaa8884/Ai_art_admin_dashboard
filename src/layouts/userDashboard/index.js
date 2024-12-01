eff7db706bcd947f90dad80f1d09f49efdbe5afb
Alaa Mohammed Youssef <alaamuhamed8@gamil.com>
2024-12-01 17:17:40 +0300

    Intinal Adding admin dashboard

---
+++ src/layouts/userDashboard/index.js
@@ -0,0 +1,44 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// @mui icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+
+function UserDashboard() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox mb={55} />
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default UserDashboard;
